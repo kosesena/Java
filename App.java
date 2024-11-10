bb6ca6d6ebeacb9257526f1a5b914f6d1d9316b2
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-10 20:56:09 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,29 @@
+public class App {
+    /* Pasta yapım süreci
+     * Bu algoritma bir pasta yapım sürecini adımlara böler.
+     
+     * ALGORİTMA
+     * 1.Malzemeleri hazırlayın(un,yumurta,şeker,süt vb.)
+     * 2.Fırını önceden ısıtın(180 derece)
+     * 3.Un,şeker ve yumurtaları karıştırın
+     * 4.Karışıma süt ekleyin ve çırpın.
+     * 5.Karışımı pasta kabına dökün.
+     * 6.Önceden ısıtılmış fırına koyun ve pişirin.
+     * 7.Piştikten sonra pastayı çıkarıp soğumaya bırakın.
+     * 8.Süslemeleri ekleyin(isteğe bağlı)
+     * 9.Pastayı servis edin.
+     
+    */
+    public static void main(String[] args) {
+        System.out.println("Pasta yapım süreci");
+        System.out.println("1.Malzemeleri hazırlayın(un,yumurta,şeker,süt vb.)");
+        System.out.println("2.Fırını önceden ısıtın(180 derece)");
+        System.out.println("3.Un,şeker ve yumurtaları karıştırın");
+        System.out.println("4.Karışıma süt ekleyin ve çırpın.");
+        System.out.println("5.Karışımı pasta kabına dökün.");
+        System.out.println("6.Önceden ısıtılmış fırına koyun ve pişirin.");
+        System.out.println("7.Piştikten sonra pastayı çıkarıp soğumaya bırakın.");
+        System.out.println("8.Süslemeleri ekleyin(isteğe bağlı)");
+        System.out.println("9.Pastayı servis edin.");
+    }
+}
