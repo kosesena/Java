c8f643629ae85f4b20d92641405d8c6f1c91c5d8
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-10 22:10:47 +0300

    Update Faktoriyel.java

--- Faktoriyel.java
+++ Faktoriyel.java
@@ -1,21 +1,22 @@
 package Soru1;
 import java.util.Scanner;
 /* 
- * Bir sayının faktöriyelini bulan algoritma
- * Faktöriyel: Bir sayının faktöriyeli,1'den o sayıya kadar olan tüm pozitif tam sayıların çarpımıdır.
- * 
- * Örneğin, 5! = 5x4x3x2x1 = 120
- * 
- * ALGORİTMA:
- * 
- * 1.Başla
- * 2.Kullanıcıdan bir sayı al(n).
- * 3.Faktöriyel değişkenini 1 olarak başlat.
- * 4.Sayı 1'den büyükse, 1'den n'ye kadar döngü oluştur.
- * Her adımda faktöriyel değişkenini güncelle: faktöriyel=faktöriyel * i
- * 5.Faktöriyel değerini yazdır.
- * 6.Bitir.
- *
+  Bir sayının faktöriyelini bulan algoritma
+ 
+  Faktöriyel: Bir sayının faktöriyeli,1'den o sayıya kadar olan tüm pozitif tam sayıların çarpımıdır.
+  
+  Örneğin, 5! = 5x4x3x2x1 = 120
+  
+  ALGORİTMA:
+  
+  1.Başla
+  2.Kullanıcıdan bir sayı al(n).
+  3.Faktöriyel değişkenini 1 olarak başlat.
+  4.Sayı 1'den büyükse, 1'den n'ye kadar döngü oluştur.
+  Her adımda faktöriyel değişkenini güncelle: faktöriyel=faktöriyel * i
+  5.Faktöriyel değerini yazdır.
+  6.Bitir.
+ 
  */
 
 public class Faktoriyel {
