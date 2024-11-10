c497869a1ca5f43a8bf370b626cb89314f12b6d0
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-10 22:12:36 +0300

    Update Cake.java

--- Cake.java
+++ Cake.java
@@ -1,17 +1,21 @@
 public class App {
-    /* Pasta yapım süreci
-     * Bu algoritma bir pasta yapım sürecini adımlara böler.
+    /* 
+   
+    Pasta yapım süreci
+    
+    Bu algoritma bir pasta yapım sürecini adımlara böler.
      
-     * ALGORİTMA
-     * 1.Malzemeleri hazırlayın(un,yumurta,şeker,süt vb.)
-     * 2.Fırını önceden ısıtın(180 derece)
-     * 3.Un,şeker ve yumurtaları karıştırın
-     * 4.Karışıma süt ekleyin ve çırpın.
-     * 5.Karışımı pasta kabına dökün.
-     * 6.Önceden ısıtılmış fırına koyun ve pişirin.
-     * 7.Piştikten sonra pastayı çıkarıp soğumaya bırakın.
-     * 8.Süslemeleri ekleyin(isteğe bağlı)
-     * 9.Pastayı servis edin.
+    ALGORİTMA:
+     
+     1.Malzemeleri hazırlayın(un,yumurta,şeker,süt vb.)
+     2.Fırını önceden ısıtın(180 derece)
+     3.Un,şeker ve yumurtaları karıştırın
+     4.Karışıma süt ekleyin ve çırpın.
+     5.Karışımı pasta kabına dökün.
+     6.Önceden ısıtılmış fırına koyun ve pişirin.
+     7.Piştikten sonra pastayı çıkarıp soğumaya bırakın.
+     8.Süslemeleri ekleyin(isteğe bağlı)
+     9.Pastayı servis edin.
      
     */
     public static void main(String[] args) {
