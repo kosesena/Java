0d5a227bb8fe6e217e440558b0a230cd85cbeeb1
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-10 21:52:09 +0300

    Add files via upload

---
+++ OgrenciNotu.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/* Öğrencinin numarasını, vize ve final notunu aldıktan sonra ortalamasını hesaplayıp numarasını ve not ortalamasını yazdıran program */
+
+public class OgrenciNotu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Öğrenci numarasını girin: ");
+        int numara = scanner.nextInt();
+
+        System.out.print("Vize notunu girin: ");
+        double vize = scanner.nextDouble();
+
+        System.out.print("Final notunu girin: ");
+        double finalNotu = scanner.nextDouble();
+
+        double ortalama = (vize * 0.4) + (finalNotu * 0.6);
+
+        System.out.println("Öğrenci Numarası: " + numara);
+        System.out.println("Ortalama: " + ortalama);
+    }
+}
