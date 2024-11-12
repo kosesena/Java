e4fe8da197cb0aa3a20f3da704f593015d628eff
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-12 13:33:35 +0300

    Add files via upload

---
+++ TekCiftKontrol.java
@@ -0,0 +1,22 @@
+
+/* Klavyeden girilen pozitif bir tamsayının tek mi çift mi olduğunu tespit eden program */
+
+
+import java.util.Scanner;
+
+public class TekCiftKontrol {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bir pozitif tamsayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        if (sayi % 2 == 0) {
+            System.out.println("Girdiğiniz sayı çifttir.");
+        } else {
+            System.out.println("Girdiğiniz sayı tektir.");
+        }
+
+        scanner.close();
+    }
+}
