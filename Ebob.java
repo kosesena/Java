8c49b77fddad65b590016f739a9a13807af0e488
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-14 18:38:44 +0300

    Update Ebob.java

--- Ebob.java
+++ Ebob.java
@@ -19,24 +19,29 @@ public class App {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-        
+       
+        // İlk sayıyı kullanıcıdan alıyoruz
         System.out.print("İlk sayıyı girin: ");
-        
         int a = scanner.nextInt();
-        
+
+       // İkinci sayıyı kullanıcıdan alıyoruz
         System.out.print("İkinci sayıyı girin: ");
-        
         int b = scanner.nextInt();
-
+       
+       // Girilen sayılardan küçük olanını buluyoruz
         int min = Math.min(a, b);
-        int ebob = 1;
+        int ebob = 1;// EBOB'u saklamak için bir değişken
 
+      //min değerden başlayarak 1'e kadar geri sayıyoruz
         for (int i = min; i >= 1; i--) {
-            if (a % i == 0 && b % i == 0) {
-                ebob = i;
-                break;
+            
+          // Hem a hem de b, i'ye tam bölünüyorsa
+          if (a % i == 0 && b % i == 0) {
+                ebob = i; // EBOB'u i olarak belirliyoruz
+                break; // Döngüyü sonlandırıyoruz çünkü EBOB'u bulduk
             }
         }
+         // Sonucu ekrana yazdırıyoruz
           System.out.println("EBOB: " + ebob);
     }
 
