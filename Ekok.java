12cd2b45e37cad5167c5403b687a63ab38c523a1
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-11-14 18:47:09 +0300

    Update Ekok.java

--- Ekok.java
+++ Ekok.java
@@ -1,29 +1,34 @@
+ /* Girilen iki sayının en küçük ortak katını (ekok) bulan algoritma */
+
 import java.util.Scanner;
 
 public class Ekok {
     
-    /* Girilen iki sayının en küçük ortak katını (ekok) bulan algoritma */
 
     public static void main(String[] args)  {
         
         Scanner scanner = new Scanner(System.in);
-        
+     // İlk sayıyı kullanıcıdan alıyoruz
         System.out.println("Enter the first number: ");
-        
         int a = scanner.nextInt();
-       
+        
+     // İkinci sayıyı kullanıcıdan alıyoruz
         System.out.println("Enter the second number: ");
-       
         int b = scanner.nextInt();
         
+     // Başlangıç değeri olarak, a ve b'den büyük olan sayıyı alıyoruz
         int max = Math.max(a, b);
 
+     // Sonsuz döngü başlatıyoruz. EKOK bulunana kadar döngü devam edecek
         while (true) {
+            
+        // Eğer max hem a'ya hem de b'ye tam bölünüyorsa, max EKOK'tur.
             if (max % a == 0 && max % b == 0) {
-                System.out.println("EKOK:  " + max);
-                break;
+                System.out.println("EKOK:  " + max);// EKOK'u yazdırıyoruz.
+               
+                break; // Döngüyü sonlandırıyoruz.
             }
-            max++;
+            max++; // max değerini 1 arttırarak kontrol etmeye devam ediyoruz.
             
         }
         
