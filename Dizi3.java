2404d9a6d669c168e17c653cc511e4d62052f787
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-12-12 00:03:23 +0300

    Update Dizi3.java

--- Dizi3.java
+++ Dizi3.java
@@ -1,10 +1,10 @@
 /* Kullanıcı tarafından tam sayı değerleri girilen 20 elemanlı bir diziyi küçükten büyüğe sıralayıp konsolda gösteren Java programını yazınız.*/
     
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Arrays; // Diziler üzerinde işlemler yapmak için kullanabileceğimiz Arrays sınıfını içe aktarır.
+import java.util.Scanner; // Kullanıcıdan veri almak için kullanılan Scanner sınıfını içe aktarır.
 
-public class Main {
+public class Dizi3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[20];
@@ -21,7 +21,7 @@ public class Main {
 
         // Sıralanmış diziyi ekrana yazdırma
         System.out.println("Dizi küçükten büyüğe sıralandı:");
-        for (int num : numbers) {
+        for (int num : numbers) { 
             System.out.print(num + " ");
         }
     }
