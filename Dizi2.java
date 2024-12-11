fca6b435fb8b5abf9fb08e894955379ac2c1d2bf
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-12-11 23:33:36 +0300

    Update Dizi2.java

--- Dizi2.java
+++ Dizi2.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[20];
+        Scanner scanner = new Scanner(System.in); // Bu satır,kullanıcıdan veri almaya sağlayan bir Scanner nesnesi oluşturur.System.in ifadesi,klavyeden giriş yapılacağını belirtir.
+        int[] numbers = new int[20];  // Burada,20 elemanlı bir tam sayı(int)dizisi tanımlanır.Bu diziye kullanıcı tarafından girilecek sayılar kaydedilecektir.
 
         System.out.println("Lütfen 20 tam sayı giriniz:");
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print((i + 1) + ". sayı: ");
-            numbers[i] = scanner.nextInt();
+            System.out.print((i + 1) + ". sayı: "); // Her iterasyonda,kullanıcıdan bir sayı girmesi istenir.(i+1) ifadesi,kullanıcının kaçıncı sayıyı girdiğini anlaması için yazdırılır.
+            numbers[i] = scanner.nextInt();//Kullanıcının girdiği sayı,dizinin i indeksine kaydedilir.scanner.nextInt() ifadesi,kullanıcının klavyeden girdiği tam sayıyı okur.
         }
 
         int minValue = numbers[0];
