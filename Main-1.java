d67b8b32468a9be1d5e7f07467c16294e20945fc
Sena Köse <147408306+kosesena@users.noreply.github.com>
2024-12-11 23:24:31 +0300

    Add files via upload

---
+++ Main-1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[20];
+
+        System.out.println("Lütfen 20 tam sayı giriniz:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print((i + 1) + ". sayı: ");
+            numbers[i] = scanner.nextInt();
+        }
+
+        int minValue = numbers[0];
+        int minIndex = 0;
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < minValue) {
+                minValue = numbers[i];
+                minIndex = i;
+            }
+        }
+
+        System.out.println("En küçük eleman: " + minValue);
+        System.out.println("En küçük elemanın indeksi: " + minIndex);
+    }
+}
+
