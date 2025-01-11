38ead088f89cec3cba32a9a9d87e136865982556
Sena Köse <147408306+kosesena@users.noreply.github.com>
2025-01-11 17:23:06 +0300

    Create Diziii.java

---
+++ Diziii.java
@@ -0,0 +1,14 @@
+/*
+Aşağıdaki a matrisinin satır ve sütun toplamlarını döngüyle toplayan ve yeni bir b matrisi oluşturup bu değerleri ona atayan ve konsolda gösteren Java programını yazınız?
+     a                             b
+100 20 300                  100 20 300  420
+44 55 666                   44  55 666  765
+7 80 9                       7  80 9    96
+10 11 12                     10 11 12   33
+                           161 166 987 2628
+  */
+
+                    
+
+                     
+                    
