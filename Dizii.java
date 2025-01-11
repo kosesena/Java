730f95f4c2441abf2b99347a563f45347373d07c
Sena Köse <147408306+kosesena@users.noreply.github.com>
2025-01-11 17:11:08 +0300

    Update Dizii.java

--- Dizii.java
+++ Dizii.java
@@ -1,6 +1,5 @@
 /*
-Aşağıdaki matrisi küçükten büyüğe doğru sıralayıp yine matris
-formunda konsolda yazan Java programını yazınız?
+Aşağıdaki matrisi küçükten büyüğe doğru sıralayıp yine matris formunda konsolda yazan Java programını yazınız?
 100 20 300
 44 55 666
 7 80 9
