ef852b6c8a6ffe4a488b6bff2320a360be4ca65a
Sena Köse <147408306+kosesena@users.noreply.github.com>
2025-01-11 17:10:56 +0300

    Update Dizi9.java

--- Dizi9.java
+++ Dizi9.java
@@ -1,6 +1,5 @@
 /*
-Aşağıdaki matrisi tek boyutlu bir diziye 1) satır satır, 2) sütun
-sütun ekleyip konsolda yazan Java programını yazınız?
+Aşağıdaki matrisi tek boyutlu bir diziye 1) satır satır, 2) sütun sütun ekleyip konsolda yazan Java programını yazınız?
 100 20 300
 44 55 666
 7 80 9
