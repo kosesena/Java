f8b72fec45bc4b57574553f4727fff62b05d4edb
Sena Köse <147408306+kosesena@users.noreply.github.com>
2025-01-11 17:23:28 +0300

    Update Diziii.java

--- Diziii.java
+++ Diziii.java
@@ -3,7 +3,7 @@ Aşağıdaki a matrisinin satır ve sütun toplamlarını döngüyle toplayan ve
      a                             b
 100 20 300                  100 20 300  420
 44 55 666                   44  55 666  765
-7 80 9                       7  80 9    96
+ 7 80 9                      7  80 9    96
 10 11 12                     10 11 12   33
                            161 166 987 2628
   */
